adfa0a99268a8c580a8d0e5058fb0dd75971c081
Alexander Danilov <h4rb4rd@gmail.com>
2022-04-25 21:38:42 +0300

    feat: add meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,28 @@
+import Head from 'next/head'
+
 import A from '../components/A'
 
 const Index = () => {
 	return (
-		<div>
-			<div className='navbar'>
-				<A href='/' text='Главная' />
-				<A href='/users' text='Пользователи' />
+		<>
+			<Head>
+				<meta keywords='nextjs' />
+				<title>Главная страница</title>
+			</Head>
+			<div>
+				<div className='navbar'>
+					<A href='/' text='Главная' />
+					<A href='/users' text='Пользователи' />
+				</div>
+				<h1>Главная страница</h1>
+				<style jsx>{`
+					.navbar {
+						background: orange;
+						padding: 15px;
+					}
+				`}</style>
 			</div>
-			<h1>Главная страница</h1>
-			<style jsx>{`
-				.navbar {
-					background: orange;
-					padding: 15px;
-				}
-			`}</style>
-		</div>
+		</>
 	)
 }
 
